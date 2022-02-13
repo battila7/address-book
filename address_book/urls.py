adcdd1c10dca9a1c2404f3a17b320546d66f22ba
Attila Bagossy <bagossyattila@outlook.com>
2022-02-13 22:17:44 +0100

    feat: urls ready to roll for token-based auth

--- address_book/urls.py
+++ address_book/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt import views as jwt_views
 
 from .address import views as address_views
 from .user import views as user_views
+
 
 urlpatterns = [
     path("addresses/", address_views.AddressList.as_view(), name="address-list"),
@@ -15,6 +17,8 @@ urlpatterns = [
         address_views.AddressDetail.as_view(),
         name="address-detail",
     ),
+    path("token/", jwt_views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", jwt_views.TokenRefreshView.as_view(), name="token_refresh"),
     path(
         "user/",
         user_views.UserSelfDetail.as_view(),
