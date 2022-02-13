1dd89b7748e30562ecb855831c261c1375094083
Attila Bagossy <bagossyattila@outlook.com>
2022-02-13 21:06:40 +0100

    feat: disallowing duplicate addresses

--- address_book/address/models.py
+++ address_book/address/models.py
@@ -13,15 +13,23 @@ class Address(models.Model):
     state/province and two address lines.
 
     Also, I embedded the country-state-zip-city four-tuple in this model,
-    although, one could argue that it should be represented separately. Since
-    our application is a simple one, I wanted to go with a simpler approach.
+    even though it somewhat violates DB normalization rules. Since
+    our application is a simple one, I wanted to go with the simplest approach
+    possible, which was definitely denormalization and plain text fields.
+
+    Considering the duplicates requirement: I added a unique constraint on the
+    title and a separate one the remaining address-locating fields.
+
+    The 100 max_length is completely arbitrary, in this case, it really does not
+    matter that much. The key is the use of a CharField, as it is handled differently
+    (and more efficiently) thank a TextField.
     """
 
     COUNTRIES = [(country.alpha_2, country.name) for country in pycountry.countries]
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    title = models.CharField(max_length=100)
+    title = models.CharField(unique=True, max_length=100)
 
     country = models.CharField(choices=COUNTRIES, max_length=2)
     state = models.CharField(max_length=100)
@@ -29,3 +37,17 @@ class Address(models.Model):
     city = models.CharField(max_length=100)
     address_line_one = models.CharField(max_length=100)
     address_line_two = models.CharField(max_length=100)
+
+    class Meta:
+        # We don't need to consider the title attribute, as it
+        # has its own unique constraint.
+        unique_together = [
+            [
+                "country",
+                "state",
+                "zip_code",
+                "city",
+                "address_line_one",
+                "address_line_two",
+            ]
+        ]
