703e9bf8d66650e37bde5d640a82e88acad2eb1e
Attila Bagossy <bagossyattila@outlook.com>
2022-02-13 21:12:10 +0100

    feat: added a serializer for the Address model

---
+++ address_book/address/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from .models import Address
+
+
+class AddressSerializer(serializers.HyperlinkedModelSerializer):
+    """Serializer for Addresses.
+
+    I prefer being explicit when it comes to serialization and list fields
+    by hand.
+    """
+
+    class Meta:
+        model = Address
+        fields = (
+            "url",
+            "id",
+            "created_at",
+            "title",
+            "country",
+            "state",
+            "zip",
+            "city",
+            "address_line_one",
+            "address_line_two",
+        )
