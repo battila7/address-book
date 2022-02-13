62caf122e8fe47885ca3d07ca7e70f8e051272f0
Attila Bagossy <bagossyattila@outlook.com>
2022-02-13 22:14:39 +0100

    feat: registered simplejwt as an authentication class

--- address_book_project/settings.py
+++ address_book_project/settings.py
@@ -99,6 +99,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ],
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
